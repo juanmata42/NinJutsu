75194a6ed1ca0ea065a4e8cad49a4721e6e8905d
juanma42 <juanmanuel@gmail.com>
2022-12-05 11:22:21 +0100

    some styling

--- src/main/home/index.jsx
+++ src/main/home/index.jsx
@@ -1,101 +1,131 @@
 import React from 'react';
 import logo from '../../assets/logo.png';
-import BujinkanLogo from './BujinkanLogo'
+import BujinkanLogo from './BujinkanLogo';
+import svgLogo from '../../assets/bujinkan-logo.svg';
 import './styles.scss';
 
 export default function home() {
-  return (<>
-  <header className="main__header">
-    <img className="main__logo" src={logo} alt="logo" />
-      <h1 className="main__title">Nin Jutsu en Madrid</h1>
-      <div className="light-switch">
-        <button className='light-switch__bright'>
-          <BujinkanLogo />
-        </button>
-        <button className='light-switch__dark'></button>
-      </div>
-      </header>
-    <main className="main__body">
-      <div className="main__content">
-        <div className="main__content-left">
-          <p className="main__content-left__item">
-            <u>Instructor:</u>
-          </p>
-          <p className="main__content-left__description">
-            Javier Alfonso Tundidor
-          </p>
-          <p className="main__content-left__description">
-            Email: <a href="mailto:javier@ninjutsumadrid.es">Javier</a>
-          </p>
-          <p className="main__content-left__item">
-            <u>Horario:</u>
-          </p>
-          <p className="main__content-left__description">
-            Lunes y Miércoles de 18:30 a 19.30.
-          </p>
-          <p className="main__content-left__item">
-            <u>Gimnasio:</u>
-          </p>
-          <p className="main__content-left__description">
-            Metro: <br /> L1-Estrecho y L7-Francos Rodriguez
-          </p>
-          <iframe
-            title="map"
-            className="main__content-left__map"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d194054.94393972418!2d-3.994932370826314!3d40.539192012007064!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13 1!3m3!1m2!1s0xd422983acfd66f3%3A0x8c9074fc54ca96f5!2sClub%20Deportivo%20Tsunami!5e0!3m2!1ses!2ses!4v1667478091705!5m2!1ses!2ses"
-          ></iframe>
+  return (
+    <>
+      <header className="main__header">
+        <img className="main__logo" src={logo} alt="logo" />
+        <h1 className="main__title">Nin Jutsu en Madrid</h1>
+        <div className="light-switch">
+          <button className="light-switch__bright">
+            <img src={svgLogo} alt="logo" />
+          </button>
+          <button className="light-switch__dark">
+            <img src={svgLogo} alt="logo" />
+          </button>
         </div>
-        <div className="main__content-right">
-          <p>
-            Nin-jutsu es un arte marcial tradicional, que surge en el siglo IX
-            en el Japón feudal, época con grandes inestabilidades sociales, lo
-            que permitió el desarrollo rápido de las Artes Marciales, llegaron a
-            existir hasta 73 escuelas de ninjutsu con conocimientos de
-            estrategia, infiltración y eficacia en el combate.
-          </p>
-          <p>
-            En el siglo pasado se ha empezado a conocer en occidente gracias a
-            un heredero directo de varias de estas escuelas, MASAAKI HATSUMI,
-            que lo ha transmitido a occidentales.
-          </p>
-          <p>
-            Ninjutsu / Budo Tai Jutsu es un arte marcial capaz de aunar la
-            eficacia de la defensa personal y la práctica de la tradición
-            japonesa en la línea más pura.
-          </p>
-          <p>
-            Engloba técnicas cuerpo a cuerpo de golpeo, proyecciones, luxaciones
-            y controles, técnicas con todo tipo de armas así como una parte más
-            mental como meditación y canalización de la energía.
-          </p>
-          <div className="main__content-right__container">
-            <div className="main__content-right__container-column">
-              <p><u>Gimnasio</u></p>
-              <a target="blank" href="https://www.clubtsunami.es/website/">Club Deportivo Tsunami</a>
-            </div>
-            <div className="main__content-right__container-column">
-              <p><u>Nuestra asociación</u></p>
-              <a target="blank" href="https://www.jmcollado.com/">Bujinkan Collado Dojo</a>
-            </div>
-            <div className="main__content-right__container-column">
-              <p><u>Otros Dojos en Madrid</u></p>
-              <a target="blank" href="https://es-es.facebook.com/people/Nin-Jutsu-Tres-Cantos/100050039177804/">Ninjutsu Tres Cantos</a>
-              <a target="blank" href="https://es-la.facebook.com/ninjutsurivas/">Ninjutsu Rivas Vaciamadrid</a>
-            </div>
-            <div className="main__content-right__container-column">
-              <p><u>Compra de Material</u></p>
-              <a target="blank" href="https://fujimae.com/es/tiendas">Tienda de Material en Madrid</a>
-              <a target="blank" href="https://www.budoya.es/">Budo YA artículos para Artes Marciales</a>
+      </header>
+      <main className="main__body">
+        <div className="main__content">
+          <div className="main__content-left">
+            <p className="main__content-left__item">
+              <u>Instructor:</u>
+            </p>
+            <p className="main__content-left__description">
+              Javier Alfonso Tundidor
+            </p>
+            <p className="main__content-left__description">
+              Email: <a href="mailto:javier@ninjutsumadrid.es">Javier</a>
+            </p>
+            <p className="main__content-left__item">
+              <u>Horario:</u>
+            </p>
+            <p className="main__content-left__description">
+              Lunes y Miércoles de 18:30 a 19.30.
+            </p>
+            <p className="main__content-left__item">
+              <u>Gimnasio:</u>
+            </p>
+            <p className="main__content-left__description">
+              Metro: <br /> L1-Estrecho y L7-Francos Rodriguez
+            </p>
+            <iframe
+              title="map"
+              className="main__content-left__map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d194054.94393972418!2d-3.994932370826314!3d40.539192012007064!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13 1!3m3!1m2!1s0xd422983acfd66f3%3A0x8c9074fc54ca96f5!2sClub%20Deportivo%20Tsunami!5e0!3m2!1ses!2ses!4v1667478091705!5m2!1ses!2ses"
+            ></iframe>
+          </div>
+          <div className="main__content-right">
+            <p>
+              Nin-jutsu es un arte marcial tradicional, que surge en el siglo IX
+              en el Japón feudal, época con grandes inestabilidades sociales, lo
+              que permitió el desarrollo rápido de las Artes Marciales, llegaron
+              a existir hasta 73 escuelas de ninjutsu con conocimientos de
+              estrategia, infiltración y eficacia en el combate.
+            </p>
+            <p>
+              En el siglo pasado se ha empezado a conocer en occidente gracias a
+              un heredero directo de varias de estas escuelas, MASAAKI HATSUMI,
+              que lo ha transmitido a occidentales.
+            </p>
+            <p>
+              Ninjutsu / Budo Tai Jutsu es un arte marcial capaz de aunar la
+              eficacia de la defensa personal y la práctica de la tradición
+              japonesa en la línea más pura.
+            </p>
+            <p>
+              Engloba técnicas cuerpo a cuerpo de golpeo, proyecciones,
+              luxaciones y controles, técnicas con todo tipo de armas así como
+              una parte más mental como meditación y canalización de la energía.
+            </p>
+            <div className="main__content-right__container">
+              <div className="main__content-right__container-column">
+                <p>
+                  <u>Gimnasio</u>
+                </p>
+                <a target="blank" href="https://www.clubtsunami.es/website/">
+                  Club Deportivo Tsunami
+                </a>
+              </div>
+              <div className="main__content-right__container-column">
+                <p>
+                  <u>Nuestra asociación</u>
+                </p>
+                <a target="blank" href="https://www.jmcollado.com/">
+                  Bujinkan Collado Dojo
+                </a>
+              </div>
+              <div className="main__content-right__container-column">
+                <p>
+                  <u>Otros Dojos en Madrid</u>
+                </p>
+                <a
+                  target="blank"
+                  href="https://es-es.facebook.com/people/Nin-Jutsu-Tres-Cantos/100050039177804/"
+                >
+                  Ninjutsu Tres Cantos
+                </a>
+                <a
+                  target="blank"
+                  href="https://es-la.facebook.com/ninjutsurivas/"
+                >
+                  Ninjutsu Rivas Vaciamadrid
+                </a>
+              </div>
+              <div className="main__content-right__container-column">
+                <p>
+                  <u>Compra de Material</u>
+                </p>
+                <a target="blank" href="https://fujimae.com/es/tiendas">
+                  Tienda de Material en Madrid
+                </a>
+                <a target="blank" href="https://www.budoya.es/">
+                  Budo YA artículos para Artes Marciales
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-    <footer className="main__footer">
+      </main>
+      <footer className="main__footer">
         Arte Marcial con infinidad de técnicas, sea cual sea tu condición
         física, seas hombre, mujer , niño o niña a partir de 12 años. ¡¡¡VEN Y
         PRUEBA, ATRÉVETE!!!
       </footer>
-      </>
+    </>
   );
 }
