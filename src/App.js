b195003879c434334bd9185697c1292c80d056ac
SergioGonzalezIznaola <108745559+SergioGonzalezIznaola@users.noreply.github.com>
2023-01-29 10:24:02 +0100

    change_theme_all

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import "./Styles/Styles.scss";
 import { useState } from "react";
 
 function App() {
+  
   const [theme, setTheme] = useState('dark');
+
   function changeTheme (){
     if (theme === "dark"){
       setTheme("bright")
@@ -16,7 +18,7 @@ function App() {
   console.log(theme)
   return (
     <div className={`app ${theme}-mode`}>
-      <Home value={changeTheme}/>
+      <Home className={`main-${theme}`} value={changeTheme}/>
     </div>
   );
 }
