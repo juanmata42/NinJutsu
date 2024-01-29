697338f3a352104d737a4fce937a1142c48996b9
Juan Manuel Taboada <juanmanuel.taboada@globalincubator.com>
2024-01-29 12:30:34 +0100

    cleanup and carousel done

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // This line should be included
+import './carouselStyles.scss';
+
 
 const CarouselComponent = () => {
-  // Fetch all images from the folder dynamically
+  const [centerSlidePercentage, setCenterSlidePercentage] = useState(window.innerWidth < 768 ? 80 : 33);
   const imageContext = require.context('./CarouselImages', false, /\.(jpg|jpeg|png|gif|svg|JPG)$/);
-  const images = imageContext.keys().map(imageContext);
+  const images = imageContext.keys().map(key => imageContext(key));
+  useEffect(() => {
+    const handleResize = () => {
+      setCenterSlidePercentage(window.innerWidth < 768 ? 100 : 33);
+    };
 
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
   return (
-    <Carousel>
-       {images.map((image, index) => (
-        <div key={index}>
-          <img src={image.default} alt={`${index}`} />
+    <div className='carousel-container'>
+    <Carousel 
+    autoPlay
+    infiniteLoop 
+    emulateTouch
+    centerMode
+    centerSlidePercentage={centerSlidePercentage}
+    showThumbs={false} 
+    showArrows={false} 
+    showStatus={false} 
+    showIndicators={false}>
+      {images.map((image, index) => (
+        <div key={index} className='carousel-img-container'>
+          <img src={image} className='carousel-img' alt={`${index}`} />
         </div>
       ))}
     </Carousel>
+    </div>
   );
 };
 
